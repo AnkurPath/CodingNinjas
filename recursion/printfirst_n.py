5a6b9c787227b14132b658f70ccd45f847ea9367
Ankur Kumar Pathak <98033479+AnkurPath@users.noreply.github.com>
2023-05-04 22:14:39 +0530

    Delete printfirst_n.py

--- recursion/printfirst_n.py
+++
@@ -1,9 +0,0 @@
-# print numbers from 1 to n(given by user) using Recursion
-def printfirst_n(n):
-    if n==0:
-        return
-    printfirst_n(n-1)
-    print(n)
-    return
-
-print(printfirst_n(10))
